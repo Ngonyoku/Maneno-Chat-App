a54c05079318abb0f0257065d50d70afdd4fe5e4
Ngonyoku <ngonyoku001@gmail.com>
2020-08-10 21:00:14 +0300

    Friend Requests can now be Sent.

--- app/src/main/java/com/ngonyoku/maneno/ProfileActivity.java
+++ app/src/main/java/com/ngonyoku/maneno/ProfileActivity.java
@@ -16,8 +16,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -51,32 +54,72 @@ public class ProfileActivity extends AppCompatActivity {
         mFriends = findViewById(R.id.profile_friends);
         mAddFriendButton = findViewById(R.id.profile_add_friend);
 
-        mCurrent_state = "not_friend";
+        mCurrent_state = getString(R.string.not_friend);
 
-        /*Load the Data inside the Views*/
+        /*-----------Load the Data inside the Views-----------------------*/
         if (getIntent().getExtras() != null) {
             user_id = getIntent().getStringExtra(UsersActivity.KEY_USER_ID);
             mDisplayName.setText(getIntent().getStringExtra(UsersActivity.KEY_DISPLAY_NAME));
             mStatus.setText(getIntent().getStringExtra(UsersActivity.KEY_STATUS));
-            Picasso
-                    .get()
-                    .load(getIntent().getStringExtra(UsersActivity.KEY_IMAGE_URL))
-                    .fit()
-                    .into(mDisplayImage)
-            ;
+            String imageUrl = getIntent().getStringExtra(UsersActivity.KEY_IMAGE_URL);
+            if (!imageUrl.isEmpty()) {
+                Picasso
+                        .get()
+                        .load(imageUrl)
+                        .fit()
+                        .into(mDisplayImage)
+                ;
+            } else {
+                Toast.makeText(this, "User doesn't have an Image", Toast.LENGTH_SHORT).show();
+            }
         }
 
-        /*Send a Friend Request*/
+        /*-------------------- Detect if a user is a Friend or not-----------------------*/
+        mFriendRequestRef
+                .child(mCurrentUser.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(user_id)) {
+                            String request_type = snapshot /*We get the Request type from the database*/
+                                    .child(user_id)
+                                    .child(getString(R.string.db_field_request_type))
+                                    .getValue()
+                                    .toString();
+
+                            if (request_type.equals(getString(R.string.received))) {
+                                mCurrent_state = getString(R.string.request_received);
+                                mAddFriendButton.setText(R.string.accept_friend_request);
+                                mAddFriendButton.setBackground(getDrawable(R.color.colorGreen));
+                            } else if (request_type.equals(getString(R.string.sent))) {
+                                mCurrent_state = getString(R.string.request_sent);
+                                mAddFriendButton.setText(R.string.cancel_friend_request);
+                                mAddFriendButton.setBackground(getDrawable(R.color.colorRed));
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                })
+        ;
+
+        /*------------------- Send/Cancel Friend Request---------------------*/
         mAddFriendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mCurrent_state.equals("not_friend")) {
+                mAddFriendButton.setEnabled(false);
+
+                /*Send Friend Request*/
+                if (mCurrent_state.equals(getString(R.string.not_friend))) {
                     /*If the user is not a Friend, we can allow the Current user to send a Friend Request*/
                     mFriendRequestRef
                             .child(mCurrentUser.getUid())
                             .child(user_id)
-                            .child("request_type")
-                            .setValue("sent")
+                            .child(getString(R.string.db_field_request_type))
+                            .setValue(getString(R.string.sent))
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
@@ -84,18 +127,51 @@ public class ProfileActivity extends AppCompatActivity {
                                         mFriendRequestRef
                                                 .child(user_id)
                                                 .child(mCurrentUser.getUid())
-                                                .child("request_type")
-                                                .setValue("received")
+                                                .child(getString(R.string.db_field_request_type))
+                                                .setValue(getString(R.string.received))
                                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                     @Override
                                                     public void onSuccess(Void aVoid) {
-                                                        Toast.makeText(ProfileActivity.this, "Request Sent", Toast.LENGTH_SHORT).show();
+                                                        mAddFriendButton.setEnabled(true);
+                                                        mCurrent_state = getString(R.string.request_sent); /*Change the current state to request_sent*/
+                                                        mAddFriendButton.setText(R.string.cancel_friend_request);
+                                                        mAddFriendButton.setBackground(getDrawable(R.color.colorRed));
+//                                                        Toast.makeText(ProfileActivity.this, "Request Sent", Toast.LENGTH_SHORT).show();
                                                     }
                                                 })
                                         ;
                                     } else {
                                         Toast.makeText(ProfileActivity.this, "Failed Sending Request", Toast.LENGTH_SHORT).show();
                                     }
+                                }
+                            })
+                    ;
+                }
+
+                /*Cancel Friend Request*/
+                if (mCurrent_state.equals(getString(R.string.request_sent))) {
+                    mFriendRequestRef
+                            .child(mCurrentUser.getUid())
+                            .child(user_id)
+                            .removeValue()
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    mFriendRequestRef
+                                            .child(user_id)
+                                            .child(mCurrentUser.getUid())
+                                            .removeValue()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    mAddFriendButton.setEnabled(true);
+                                                    mCurrent_state = getString(R.string.not_friend); /*Change the current state to request_sent*/
+                                                    mAddFriendButton.setText(R.string.add_friend);
+                                                    mAddFriendButton.setBackground(getDrawable(R.color.colorAccent));
+                                                    Toast.makeText(ProfileActivity.this, "Request Cancelled", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                    ;
                                 }
                             })
                     ;
